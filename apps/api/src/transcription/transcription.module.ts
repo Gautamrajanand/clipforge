51c6b9068848fdcbc3aa5170ffe06e8ff16958d2
ClipForge Team <dev@clipforge.dev>
2025-11-13 19:09:37 +0530

    fix: Import VideoModule into TranscriptionModule for reframe processing

--- apps/api/src/transcription/transcription.module.ts
+++ apps/api/src/transcription/transcription.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TranscriptionService } from './transcription.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { StorageModule } from '../storage/storage.module';
+import { VideoModule } from '../video/video.module';
 
 @Module({
-  imports: [PrismaModule, StorageModule],
+  imports: [PrismaModule, StorageModule, VideoModule],
   providers: [TranscriptionService],
   exports: [TranscriptionService],
 })
